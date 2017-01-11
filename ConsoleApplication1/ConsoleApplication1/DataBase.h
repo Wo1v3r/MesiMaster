795a175392f2f57e42197ac816f55ab54a5cda81
Liran Rotenberg <liranr23@gmail.com>
2017-01-12 00:44:26 +0200

    init is working(done+check), the texts files that i checked with

--- ConsoleApplication1/ConsoleApplication1/DataBase.h
+++ ConsoleApplication1/ConsoleApplication1/DataBase.h
@@ -7,6 +7,7 @@
 #define GlobalFilePath "./Databases/global.txt"
 #define QuotesFilePath "./Databases/quotes.txt"
 #define ProjectsFilePath "./Databases/projects.txt"
+#define TasksFilePath "./Databases/tasks.txt"
 /// end defines & includes 
 
 // initializations from files
@@ -18,17 +19,9 @@ Student* initStudents(){
 		return NULL;
 	}
 	else{
-		int num = 0;
 		while (!feof(read)){
-			if (num == 0)
-			{
-				newStud = (Student*)(malloc(sizeof(Student)));
-				head = newStud;
-			}
-			else{
-				newStud = (Student*)(malloc(sizeof(Student)));
-				head = AddStudent(head, newStud);					// use linked list func's to add new element to end of linked list
-			}
+			newStud = (Student*)(malloc(sizeof(Student)));
+			head = AddStudent(head, newStud);
 			fscanf(read, "%d", &newStud->StudentID);
 			fscanf(read, "%s", &newStud->StudentUsername);
 			fscanf(read, "%s", &newStud->StudentPassword);
@@ -36,7 +29,7 @@ Student* initStudents(){
 			fscanf(read, "%s", &newStud->StudentSurename);
 			fscanf(read, "%s", &newStud->StudentEmail);
 			fscanf(read, "%s", &newStud->StudentDepartment);
-			fscanf(read, "%c", &newStud->StudentYear);
+			fscanf(read, " %c", &newStud->StudentYear);
 			fscanf(read, "%s", &newStud->StudentActivityLog);
 			fscanf(read, "%d", &newStud->Group);
 			fscanf(read, "%d", &newStud->StudentProjectsAmount);
@@ -47,14 +40,13 @@ Student* initStudents(){
 			}
 			fscanf(read, "%d", &newStud->StudentTasksAmount);
 			fscanf(read, "%s", &newStud->StudentMessages);
-			num++;
 		}
-
 	}
 	fclose(read);
 	return head;
 }
-Project* initProjects(Student* studhead, Task* taskhead){
+
+Project* initProjects(){
 	Project* head = NULL, *newProj;
 	FILE * read = fopen(ProjectsFilePath, "r");
 	if (read == NULL){
@@ -62,38 +54,31 @@ Project* initProjects(Student* studhead, Task* taskhead){
 		return NULL;
 	}
 	else{
-		int num = 0;
 		while (!feof(read)){
-			if (num == 0){
-				newProj = (Project*)(malloc(sizeof(Project)));
-				head = newProj;
-			}
-			else{
-				newProj = (Project*)(malloc(sizeof(Project)));
-				head = AddProject(head, newProj);
-			}
+			newProj = (Project*)(malloc(sizeof(Project)));
+			head = AddProject(head, newProj);
 			fscanf(read, "%d", &newProj->ProjectID);
 			fscanf(read, "%s", &newProj->ProjectName);
 			fscanf(read, "%s", &newProj->ProjectCreatorName);
 			fscanf(read, "%s", &newProj->ProjectActivityLogs);
 			fscanf(read, "%d", &newProj->ProjectUsersAmount);
 			int i;
-			for (i = 0; i < newProj->ProjectUsersAmount; i++)
-			{
-
+			newProj->StudentsIDS = (int*)(malloc(newProj->ProjectUsersAmount * sizeof(int)));
+			for (i = 0; i < newProj->ProjectUsersAmount; i++){
+				fscanf(read, "%d", &newProj->StudentsIDS[i]);
 			}
 			fscanf(read, "%d", &newProj->ProjectTasksAmount);
-			for (i = 0; i < newProj->ProjectUsersAmount; i++)
-			{
-
+			newProj->TasksIDS = (int*)(malloc(newProj->ProjectTasksAmount * sizeof(int)));
+			for (i = 0; i < newProj->ProjectTasksAmount; i++){
+				fscanf(read, "%d", &newProj->TasksIDS[i]);
 			}
 			fscanf(read, "%s", &newProj->ProjectMessages);
-			num++;
 		}
 	}
 	fclose(read);
 	return head;
 }
+
 Watcher* initWatchers(){
 	Watcher* head = NULL, *newWatcher;
 	FILE * read = fopen(WatchersFilePath, "r");
@@ -102,33 +87,29 @@ Watcher* initWatchers(){
 		return NULL;
 	}
 	else{
-		int num = 0;
 		while (!feof(read)){
-			if (num == 0){
-				newWatcher = (Watcher*)(malloc(sizeof(Watcher)));
-				head = newWatcher;
-			}
-			else{
-				newWatcher = (Watcher*)(malloc(sizeof(Watcher)));
-				head = AddWatcher(head, newWatcher);
-
-			}
+			newWatcher = (Watcher*)(malloc(sizeof(Watcher)));
+			head = AddWatcher(head, newWatcher);
 			fscanf(read, "%d", &newWatcher->WatcherID);
 			fscanf(read, "%s", &newWatcher->WatcherUsername);
 			fscanf(read, "%s", &newWatcher->WatcherPassword);
 			fscanf(read, "%s", &newWatcher->WatcherName);
 			fscanf(read, "%s", &newWatcher->WatcherSurename);
 			fscanf(read, "%s", &newWatcher->WatcherEmail);
-			fscanf(read, "%s", &newWatcher->Group);
+			fscanf(read, "%d", &newWatcher->Group);
 			fscanf(read, "%d", &newWatcher->WatcherReceiveChanges);
-			fscanf(read, "%s", &newWatcher->WatcherProjectsAmount);
-
-			num++;
+			fscanf(read, "%d", &newWatcher->WatcherProjectsAmount);
+			int i;
+			newWatcher->ProjectIDS = (int*)malloc(newWatcher->WatcherProjectsAmount * sizeof(int));
+			for (i = 0; i < newWatcher->WatcherProjectsAmount; i++){
+				fscanf(read, "%d", &newWatcher->ProjectIDS[i]);
+			}
 		}
 	}
 	fclose(read);
 	return head;
 }
+
 Admin* initAdmins(){
 	Admin* head = NULL, *newAdmin;
 	FILE * read = fopen(AdminsFilePath, "r");
@@ -137,83 +118,67 @@ Admin* initAdmins(){
 		return NULL;
 	}
 	else{
-		int num = 0;
 		while (!feof(read)){
-			if (num == 0){
-				newAdmin = (Admin*)(malloc(sizeof(Admin)));
-				head = newAdmin;
-			}
-			else{
-				newAdmin = (Admin*)(malloc(sizeof(Admin)));
-				head = AddAdmin(head, newAdmin);
-			}
+			newAdmin = (Admin*)(malloc(sizeof(Admin)));
+			head = AddAdmin(head, newAdmin);
 			fscanf(read, "%d", &newAdmin->AdminID);
 			fscanf(read, "%s", &newAdmin->AdminUsername);
 			fscanf(read, "%s", &newAdmin->AdminPassword);
 			fscanf(read, "%s", &newAdmin->AdminName);
 			fscanf(read, "%s", &newAdmin->AdminSurename);
 			fscanf(read, "%d", &newAdmin->Group);
-			num++;
 		}
 	}
 	fclose(read);
 	return head;
 }
+
 Task* initTasks(){
 	Task* head = NULL, *newTask;
-	FILE * read = fopen("tasks.txt", "r");
+	FILE * read = fopen(TasksFilePath, "r");
 	if (read == NULL){
 		printf("Error opening the tasks file.\n");
 		return NULL;
 	}
 	else{
-		int num = 0;
 		while (!feof(read)){
-			if (num == 0){
-				newTask = (Task*)(malloc(sizeof(Task)));
-				head = newTask;
-			}
-			else{
-				newTask = (Task*)(malloc(sizeof(Task)));
-				head = AddTask(head, newTask);
-			}
+			newTask = (Task*)(malloc(sizeof(Task)));
+			head = AddTask(head, newTask);
 			fscanf(read, "%d", &newTask->TaskID);
 			fscanf(read, "%s", &newTask->TaskName);
 			fscanf(read, "%d", &newTask->TaskStatus);
 			fscanf(read, "%s", &newTask->TaskCreatorName);
-			num++;
 		}
 	}
 	fclose(read);
 	return head;
 }
+
 Quote* initQuotes(){
 	Quote* head = NULL, *newQuote;
+	int i;
 	FILE * read = fopen(QuotesFilePath, "r");
 	if (read == NULL){
 		printf("Error opening the quotes file.\n");
 		return NULL;
 	}
 	else{
-		int num = 0;
 		while (!feof(read)){
-			if (num == 0){
-				newQuote = (Quote*)(malloc(sizeof(Quote)));
-				head = newQuote;
-			}
-			else{
-				newQuote = (Quote*)(malloc(sizeof(Quote)));
-				head = AddQuote(head, newQuote);
-			}
+			newQuote = (Quote*)(malloc(sizeof(Quote)));
+			head = AddQuote(head, newQuote);
+			i = 0;
 			fscanf(read, "%d", &newQuote->QuoteID);
-			fscanf(read, "%s", &newQuote->Quote);
-			fscanf(read, "%d", &newQuote->QuoteAuthor);
-			num++;
+			fgetc(read);
+			fgets(newQuote->Quote, sizeof(newQuote->Quote), read);
+			while (newQuote->Quote[i++] != '\n');
+			newQuote->Quote[i - 1] = '\0';
+			fscanf(read, "%s", &newQuote->QuoteAuthor);
 		}
 	}
 	fclose(read);
 	return head;
 }
+
 Global *InitDataBases()
 {
 	FILE *globalFile = fopen(GlobalFilePath, "r");
@@ -231,8 +196,6 @@ Global *InitDataBases()
 	fscanf(globalFile, "%d", &GlobalDB->WatcherRunID);
 	fscanf(globalFile, "%d", &GlobalDB->ProjectRunID);
 	fscanf(globalFile, "%d", &GlobalDB->QuoteRunID);
-	
-
 	return GlobalDB;
 }
 /*
