5236ebf3c0d5c769e35b7c679993b2e514c4579c
Alexey Silyuk <alexesi@ac.sce.ac.il>
2017-01-11 13:11:57 +0200

    up

---
+++ ConsoleApplication1/ConsoleApplication1/DataBase.h
@@ -0,0 +1,231 @@
+
+#include "Structures.h"
+/// Define path's
+#define StudentsFilePath "/Databases/students.txt"
+#define AdminsFilePath "/Databases/admins.txt"
+#define WatchersFilePath "/Databases/watchers.txt"
+#define GlobalFilePath "/Databases/global.txt"
+#define QuotesFilePath "/Databases/quotes.txt"
+#define ProjectsFilePath "/Databases/projects.txt"
+/// end defines & includes 
+
+// initializations from files
+Student* initStudents(){
+	Student* head = NULL, *newStud;
+	FILE * read = fopen(StudentsFilePath, "r");
+	if (read == NULL){
+		printf("Error opening the students file.\n");
+		return NULL;
+	}
+	else{
+		int num = 0;
+		while (!feof(read)){
+			if (num == 0)
+			{
+				newStud = (Student*)(malloc(sizeof(Student)));
+				head = newStud;
+			}
+			else{
+				newStud = (Student*)(malloc(sizeof(Student)));
+				head = AddStudent(head, newStud);					// use linked list func's to add new element to end of linked list
+			}
+			fscanf(read, "%d", &newStud->StudentID);
+			fscanf(read, "%s", &newStud->StudentUsername);
+			fscanf(read, "%s", &newStud->StudentPassword);
+			fscanf(read, "%s", &newStud->StudentName);
+			fscanf(read, "%s", &newStud->StudentSurename);
+			fscanf(read, "%s", &newStud->StudentEmail);
+			fscanf(read, "%s", &newStud->StudentDepartment);
+			fscanf(read, "%c", &newStud->StudentYear);
+			fscanf(read, "%s", &newStud->StudentActivityLog);
+			fscanf(read, "%d", &newStud->Group);
+			fscanf(read, "%d", &newStud->StudentProjectsAmount);
+			int i;
+			newStud->ProjectIDS = (int*)(malloc(newStud->StudentProjectsAmount * sizeof(int)));
+			for (i = 0; i < newStud->StudentProjectsAmount; i++){
+				fscanf(read, "%d", &newStud->ProjectIDS[i]);
+			}
+			fscanf(read, "%d", &newStud->StudentTasksAmount);
+			fscanf(read, "%s", &newStud->StudentMessages);
+			num++;
+		}
+
+	}
+	fclose(read);
+	return head;
+}
+Project* initProjects(Student* studhead, Task* taskhead){
+	Project* head = NULL, *newProj;
+	FILE * read = fopen(ProjectsFilePath, "r");
+	if (read == NULL){
+		printf("Error opening the projects file.\n");
+		return NULL;
+	}
+	else{
+		int num = 0;
+		while (!feof(read)){
+			if (num == 0){
+				newProj = (Project*)(malloc(sizeof(Project)));
+				head = newProj;
+			}
+			else{
+				newProj = (Project*)(malloc(sizeof(Project)));
+				head = AddProject(head, newProj);
+			}
+			fscanf(read, "%d", &newProj->ProjectID);
+			fscanf(read, "%s", &newProj->ProjectName);
+			fscanf(read, "%s", &newProj->ProjectCreatorName);
+			fscanf(read, "%s", &newProj->ProjectActivityLogs);
+			fscanf(read, "%d", &newProj->ProjectUsersAmount);
+			int i;
+			for (i = 0; i < newProj->ProjectUsersAmount; i++)
+			{
+
+			}
+			fscanf(read, "%d", &newProj->ProjectTasksAmount);
+			for (i = 0; i < newProj->ProjectUsersAmount; i++)
+			{
+
+			}
+			fscanf(read, "%s", &newProj->ProjectMessages);
+			num++;
+		}
+	}
+	fclose(read);
+	return head;
+}
+Watcher* initWatchers(){
+	Watcher* head = NULL, *newWatcher;
+	FILE * read = fopen(WatchersFilePath, "r");
+	if (read == NULL){
+		printf("Error opening the admins file.\n");
+		return NULL;
+	}
+	else{
+		int num = 0;
+		while (!feof(read)){
+			if (num == 0){
+				newWatcher = (Watcher*)(malloc(sizeof(Watcher)));
+				head = newWatcher;
+			}
+			else{
+				newWatcher = (Watcher*)(malloc(sizeof(Watcher)));
+				head = AddWatcher(head, newWatcher);
+
+			}
+			fscanf(read, "%d", &newWatcher->WatcherID);
+			fscanf(read, "%s", &newWatcher->WatcherUsername);
+			fscanf(read, "%s", &newWatcher->WatcherPassword);
+			fscanf(read, "%s", &newWatcher->WatcherName);
+			fscanf(read, "%s", &newWatcher->WatcherSurename);
+			fscanf(read, "%s", &newWatcher->WatcherEmail);
+			fscanf(read, "%s", &newWatcher->Group);
+			fscanf(read, "%d", &newWatcher->WatcherReceiveChanges);
+			fscanf(read, "%s", &newWatcher->WatcherProjectsAmount);
+
+			num++;
+		}
+	}
+	fclose(read);
+	return head;
+}
+Admin* initAdmins(){
+	Admin* head = NULL, *newAdmin;
+	FILE * read = fopen(AdminsFilePath, "r");
+	if (read == NULL){
+		printf("Error opening the admins file.\n");
+		return NULL;
+	}
+	else{
+		int num = 0;
+		while (!feof(read)){
+			if (num == 0){
+				newAdmin = (Admin*)(malloc(sizeof(Admin)));
+				head = newAdmin;
+			}
+			else{
+				newAdmin = (Admin*)(malloc(sizeof(Admin)));
+				head = AddAdmin(head, newAdmin);
+			}
+			fscanf(read, "%d", &newAdmin->AdminID);
+			fscanf(read, "%s", &newAdmin->AdminUsername);
+			fscanf(read, "%s", &newAdmin->AdminPassword);
+			fscanf(read, "%s", &newAdmin->AdminName);
+			fscanf(read, "%s", &newAdmin->AdminSurename);
+			fscanf(read, "%d", &newAdmin->Group);
+			num++;
+		}
+	}
+	fclose(read);
+	return head;
+}
+Task* initTasks(){
+	Task* head = NULL, *newTask;
+	FILE * read = fopen("tasks.txt", "r");
+	if (read == NULL){
+		printf("Error opening the tasks file.\n");
+		return NULL;
+	}
+	else{
+		int num = 0;
+		while (!feof(read)){
+			if (num == 0){
+				newTask = (Task*)(malloc(sizeof(Task)));
+				head = newTask;
+			}
+			else{
+				newTask = (Task*)(malloc(sizeof(Task)));
+				head = AddTask(head, newTask);
+			}
+			fscanf(read, "%d", &newTask->TaskID);
+			fscanf(read, "%s", &newTask->TaskName);
+			fscanf(read, "%d", &newTask->TaskStatus);
+			fscanf(read, "%s", &newTask->TaskCreatorName);
+			num++;
+		}
+	}
+	fclose(read);
+	return head;
+}
+Quote* initQuotes(){
+	Quote* head = NULL, *newQuote;
+	FILE * read = fopen(QuotesFilePath, "r");
+	if (read == NULL){
+		printf("Error opening the quotes file.\n");
+		return NULL;
+	}
+	else{
+		int num = 0;
+		while (!feof(read)){
+			if (num == 0){
+				newQuote = (Quote*)(malloc(sizeof(Quote)));
+				head = newQuote;
+			}
+			else{
+				newQuote = (Quote*)(malloc(sizeof(Quote)));
+				head = AddQuote(head, newQuote);
+			}
+			fscanf(read, "%d", &newQuote->QuoteID);
+			fscanf(read, "%s", &newQuote->Quote);
+			fscanf(read, "%d", &newQuote->QuoteAuthor);
+			num++;
+		}
+	}
+	fclose(read);
+	return head;
+}
+Global *InitDataBases()
+{
+	FILE *globalFile = fopen(GlobalFilePath, "r");
+	if (!globalFile){
+		printf("Warning ! File %s can't be opened.\n", GlobalFilePath);
+		exit(1);
+	}
+	Global *GlobalDB = (Global*)malloc(sizeof(Global));
+	GlobalDB->AdminsList = initAdmins();
+	GlobalDB->StudentList = initStudents();
+	GlobalDB->WatchersList = initWatchers();
+	GlobalDB->QuotesList = initQuotes();
+
+	return GlobalDB;
+}
--- ConsoleApplication1/ConsoleApplication1/Driver.c
+++ ConsoleApplication1/ConsoleApplication1/Driver.c
@@ -1,9 +1,243 @@
 
-#include "Structures.h"
+#include "DataBase.h"
 
 int main()
 {
 	Global *GlobalFile = (Global*)malloc(sizeof(Global));
 
 	return 1;
+}
+int ProjectMenu(int projectID, int accessGroup, int userID){
+	//Showing project menu of projectID project based on the accessgroup of the user:
+	int opt = -1;
+
+	switch (accessGroup){
+		printf("0) Exit project menu\n");
+		printf("1) Create a new task\n");
+		printf("2) Print tasks list\n");
+		printf("3) Print activity log\n");
+		printf("4) Print project tasks\n");
+		printf("5) Print project details\n");
+		printf("6) Add users to Project\n");
+
+	case STUDENT:
+		printf("7) Change task status\n");
+		printf("8) Show tasks by status\n");
+		break;
+
+		while (opt != -1){
+			switch (opt){
+			case 0:
+				//Exit to upper menu
+				return 1;
+			case 1:
+
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+			default:
+				//Dosomething
+				opt = -1;
+			}
+		}
+	case ADMIN:
+
+		printf("7) Remove Project\n");
+		while (opt != -1){
+			switch (opt){
+			case 0:
+				//Exit to upper menu
+				return 1;
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+			case 7:
+				break;
+			case 8:
+				break;
+			default:
+				//Dosomething
+				opt = -1;
+			}
+			break;
+
+	case WATCHER:
+		printf("7) Change Task Status\n");
+		printf("8) Leave a message to a student\n");
+
+		while (opt != -1){
+			switch (opt){
+			case 0:
+				//Exit to upper menu
+				return 1;
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+			case 7:
+				break;
+			case 8:
+				break;
+			default:
+				//Dosomething
+				opt = -1;
+			}
+			break;
+		}
+		return 0;
+		}
+	}
+}
+
+int StudentMenu(int studentID){
+	int status = 0, opt = -1, projectID = 0;
+
+	while (opt != -1){
+
+		printf("Welcome Student:\n");
+		printf("##########################\n");
+		printf("Choose an option: (Int)\n\n");
+		printf("1) Create new project\n");
+		printf("2) Print existing projects\n");
+		printf("3) Enter Project menu\n");
+		printf("4) Show tasks by status\n");
+		printf("5) Update details\n");
+		printf("6) Print last actions\n");
+		printf("7) Exit Mesimaster\n");
+
+
+		opt = getchar();
+		switch (opt){
+
+		case 1:
+			CreateNewProject(studentID); //3
+			break;
+		case 2:
+			PrintProjectsList(studentID); // 13
+			break;
+
+		case 3:
+			printf("Enter Project ID:\n");
+			scanf("%d\n", &projectID);
+			ProjectMenu(projectID, STUDENT, studentID);
+			break;
+		case 4:
+			printf("Available Status:\n");
+			printf("[0] New , [1] Elicitation, [2] Analysis, [3] VandV, [4] Approved\n ");
+			printf("Enter an integer of your choice:\n");
+			scanf("%d\n", &status);
+			ShowTasksByStatus(studentID, status);
+			break;
+		case 5:
+			UpdateDetails(studentID); // 19
+			break;
+		case 6:
+			PrintStudentLog(studentID); // 11
+			break;
+		case 7:
+			return 1;
+		default:
+			//Dosomething
+			system("cls");
+			opt = -1;
+		}
+		system("cls");
+	}
+
+	return 0;
+
+}
+
+int AdminMenu(int ID){
+
+	printf("X) Delete User\n");
+	printf("X) Add a new User\n");
+	printf("X) Promote user to admin\n");
+	printf("X) Show user details\n");
+	printf("X) Update details\n");
+
+	return 0;
+}
+
+int WatcherMenu(int ID){
+
+	printf("X) Update details\n");
+
+	return 0;
+}
+
+
+int MainMenu(int ID){
+
+	//According to the access group, launching relevant menu:
+	int accessGroup = 0;
+	if (ID > 0 && ID <= 1000) return StudentMenu(ID);
+	else if (ID > 1000 && ID <= 2000) return AdminMenu(ID);
+	else if (ID > 2000 && ID <= 3000) return WatcherMenu(ID);
+	else printf("\n\nError Evaluating Access Group\n\n");
+	//Save();
+	//Exit();
+
+}
+
+int Login(){
+
+	return 1;
+}
+void LoginMenu(){
+
+	int ID = 0, opt = -1;
+
+	while (opt != -1){
+
+		printf("Welcome to the MesiMaster:\n");
+		printf("##########################\n");
+		printf("Choose an option: (Int)\n\n");
+		printf("1) Login with an existing user\n");
+		printf("2) Register a new user\n");
+
+		opt = getchar();
+		switch (opt){
+
+		case 1:
+			printf("Executing Login function");
+			ID = Login();
+			break;
+		case 2:
+			printf("Exexcuting Register function");
+			ID = Register();
+			break;
+		default:
+			//Dosomething
+			system("cls");
+			opt = -1;
+		}
+		system("cls");
+		if (MainMenu(ID)) LoginMenu();
+	}
 }
